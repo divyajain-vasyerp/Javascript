f8c2ec2f73df832cf11bb0f0b630d25e774f9caa
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-23 13:17:32 +0000

    added varaible

---
+++ 1.basics/1_variables.js
@@ -0,0 +1,13 @@
+console.log("Divy Jain")
+const name="Divy Kumar Jain"
+let email="Divy@vasyerp.com"
+var companyName="VasyERP"
+city="Ahmedabad"
+
+email="divyjain@vasyerp.com"
+companyName="Vasyerp"
+city="Bodakdev"
+
+// name="jain" //We can not assigned again variable value because that varible values is final
+console.log(name)
+console.table([name,email,companyName,city])
