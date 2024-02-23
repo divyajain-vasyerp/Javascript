1177aaa61002770c6681342b718f2536f1f0d3a4
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-23 13:16:16 +0000

    datatypes

---
+++ 1.basics/2_dataTypes.js
@@ -0,0 +1,14 @@
+
+let name="Divy"
+let age=18
+let isLoggedIn=false
+let last='Jain'
+let n;
+let temp=null;
+let a=""
+console.log([name,age,isLoggedIn,last,n,temp,a
+])
+console.table([name,age,isLoggedIn,last,n,temp,a])
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
