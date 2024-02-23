3adb6479ae5c2853e254ed5f0ad3f1acd0ec511b
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-23 13:49:58 +0000

    added typeConversion DataTypes

---
+++ 1.basics/3_conversionOperation.js
@@ -0,0 +1,27 @@
+// let age=13
+let age="13"
+// console.log(typeof age)
+// console.log(typeof(age))
+
+let valueInNumber=Number(age)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+let a=1;
+let boolean=Boolean(a)
+// console.log(boolean)
+// console.log(typeof boolean)
+// number-> true
+// string->true
+// emptyString->fasle
+
+
+let b=2;
+let bString=String(b)
+console.log(bString)
+console.log(typeof bString)
+// number->string
+// true->error
+// null->string
+
+
