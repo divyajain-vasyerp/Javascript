4559b3bb13b20dd33da109de727d13f2e4d18953
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-26 16:35:26 +0000

    thisandarrow functions

---
+++ 1.basics/23_this_arrow_function.js
@@ -0,0 +1,61 @@
+console.log("----This&ArrowFunctions----")
+const user={
+    name:"Divya Jain",
+    price:500,
+    welcomeMessage:function()
+    {
+        console.log(` ${this.name} ,Welcome to Websites`)
+    }
+}
+// user.welcomeMessage()
+user.name="Jain"
+// user.welcomeMessage()
+
+
+
+// function chai()
+// {
+//    let username="Divya"
+// //    console.log(this)
+// function chi() {
+//     console.log(this.username)
+//     }
+//     chi()
+// }
+
+
+// const chai=function()
+// {
+//     let username="divya Jain"
+//     console.log(username)
+// }
+
+// const chai=() =>
+// {
+//     let username="Divy"
+//     console.log(this)
+// }
+// chai()
+
+
+// sum =(n1,n2) => {
+//     return n1+n2
+// }
+// console.log(sum(1,2))
+
+// const sum=(n1,n2)=>{
+//     return n1+n2
+// }
+// console.log(sum(10,20));
+
+
+// const sum=(n1,n2)=> n1+n2
+// console.log(sum(10,20));
+
+
+// const sum=(n1,n2)=> (n1+n2)
+// console.log(sum(10,20));
+
+
+// const sum=(n1,n2)=> ({username:"Hitesh"})
+// console.log(sum(1,2))
