820063b86ab40977953d9251a0a327cbceba7182
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-26 04:43:56 +0000

    updated code

---
+++ 1.basics/13_date_time.js
@@ -0,0 +1,26 @@
+console.log("-------------")
+let date=new Date()
+// console.log(date.toDateString())
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleDateString())
+// console.log(typeof date)
+// console.log(date.toDateString())
+let myDate=new Date(2024,0,23)
+//console.log(myDate.toDateString())
+// console.log(myDate.getDate())
+let mDate=new Date("2024-02-26")
+// console.log(mDate.toDateString())
+// console.log(Date().toString())
+
+let myTimeDate=Date.now()
+// console.log(Math.floor(myTimeDate/1000))
+
+let newDate=new Date()
+// console.log(newDate.getMonth(),newDate.getDate())
+
+// console.log(`Hello Today Date is ${newDate} and My Today Month is ${newDate.getMonth()} `)
+// console.log(newDate.toLocaleString('default',{
+//     calendar:"calendar",weekday:"long",day:"2-digit"
+// }))
+
