2d2454324e19e31c32bae76bd799adcb7ffd215a
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-26 03:20:43 +0000

    Maths and Numbers

--- 1.basics/6_number&maths.js
+++ 1.basics/6_number&maths.js
@@ -1,4 +1,28 @@
 const score=400
-console.log(typeof score)
+// console.log(typeof score)
 let number1=new Number(100)
-console.log(number1)
+// console.log(number1)
+// console.log(number1.toString())
+// console.log(number1.toString().length)
+// console.log(number1.toFixed(2))
+let n2=10.894
+// console.log(n2.toPrecision(1))
+let n3=1000000
+// console.log(n3.toLocaleString('en-IN'))
+
+
+// Maths->
+// console.log(Math.PI.toFixed(2))
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.sqrt(4))
+// console.log(Math.pow(2,3))
+// console.log(Math.random())
+// console.log(Math.random()*10+1)
+// console.log((Math.random()*10)+1)
+const min=10
+const max=20
+console.log(Math.random()*(max-min+1)+min)
+console.log()
