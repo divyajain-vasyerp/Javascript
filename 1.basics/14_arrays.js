5ae051914d43bd3b20b5349971dea844b296dd69
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-26 06:31:15 +0000

    arrays is done.

---
+++ 1.basics/14_arrays.js
@@ -0,0 +1,43 @@
+console.log("------------ARRAYS---------------")
+const arr=[0,1,2,3,"Divy Jain",true]
+// console.log(arr)
+
+const myArr=new Array(1,2,3,4)
+// console.log(myArr.length) 
+// var s=" ";
+// for(var i=0;i<arr.length;i++)
+// {
+//     s=s+arr[i]+" "
+//     // console.log(arr[i])
+// }
+// console.log(s)
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(1))
+// console.log(myArr.indexOf(2))
+// let myArr1=myArr.join()
+// console.log(typeof myArr)
+// console.log(typeof myArr1)
+//slice and splice
+
+// console.log("A ",myArr)
+// const myn1=myArr.slice(1,3)
+// console.log("B ",myArr)
+// console.log(myn1)
+// const myn2=myArr.splice(1,3)
+// console.log("C ",myArr)
+// console.log(myn2)
+
+// console.log(myArr)
+// console.log(myArr.flat())
+let myar=[1,2,3,[4,5,6],7,8,9,[1,2,0],10]
+// console.log(myar)
+// console.log(myar.flat())
+// console.log(Array.of(myar))
+// console.log(Array.of("Divy"))
+// console.log(Array.from("Divy Jain"))
+
