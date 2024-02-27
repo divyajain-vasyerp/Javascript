89ca9811ec9e3dbfa7bbeb56461e38367f7ae73c
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-27 11:44:45 +0000

    loops code added

---
+++ 1.basics/27_loops.js
@@ -0,0 +1,42 @@
+console.log("---")
+const a=[10,20,30]
+// for (const i of a) {
+//     console.log(i);
+// }
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United State")
+// console.log(map);
+for (const [key,value] of map) {
+    // console.log(key, ':->',value);
+}
+
+const myObj=
+{
+    game1:'ff',
+    game2:'pubji'
+}
+
+/*for (const [key,value] of myObj) {
+    console.log(key ,value);    // this is now work for objects so we can use that time forin loops
+}*/
+
+for (const key in myObj) {
+    // console.log(myObj[key]);
+    // console.log(` ${key} shortcut is for ${myObj[key]}`)
+}
+
+const program=["js","java","c++"]
+for (const key in program) {
+    // console.log(program[key]);
+}
+program.forEach(function (val)
+{
+    // console.log(val)
+})
+
+program.forEach((item,index,arr)=>
+{
+    console.log(item,index,arr);
+})
