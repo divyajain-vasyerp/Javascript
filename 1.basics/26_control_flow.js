e47a3434a048b571046834b8af3d954f9c27cdf9
divyajain-vasyerp <156909330+divyajain-vasyerp@users.noreply.github.com>
2024-02-27 09:19:46 +0000

    swith case and conditions

---
+++ 1.basics/26_control_flow.js
@@ -0,0 +1,28 @@
+// console.log("---Control & Flow---");
+// let a=18
+// if(a>=18)
+// {
+//     console.log("true");
+// }
+// else{
+//     console.log("flase");
+// }
+
+
+// const objectEmpty={}
+// if(Object.keys(objectEmpty).length === 0)
+// {
+//     console.log("Empty");
+// }
+// else
+// {
+//     console.log("Not Empty");
+// }
+
+// let a
+// console.log(a)
+// a= 50 ??"null values" ?? "U haev a valeus"
+// console.log(a);
+
+
+
